6d3ffe98870a3ff1dd620eb70bf8631252af53e0
Adam Rosenblum <adamR714@gmail.com>
2017-09-15 21:30:55 -0700

    async

--- js/actions/index.js
+++ js/actions/index.js
@@ -1,17 +1,48 @@
 export const ADD_REPOSITORY = 'ADD_REPOSITORY';
 export const addRepository = repository => ({
     type: ADD_REPOSITORY,
-    repository
+    repository: repository
 });
 
 export const RATE_REPOSITORY = 'RATE_REPOSITORY';
 export const rateRepository = (repository, rating) => ({
     type: RATE_REPOSITORY,
-    repository,
-    rating
+    repository: repository,
+    rating: rating
 });
 
+export const FETCH_DESCRIPTION_SUCCESS = 'FETCH_DESCRIPTION_SUCCESS';
+export const fetchDescriptionSuccess = (repository, description) => ({
+    type: FETCH_DESCRIPTION_SUCCESS,
+    repository,
+    description
+});
 
+export const FETCH_DESCRIPTION_ERROR= 'FETCH_DESCRIPTION_ERROR';
+export const fetchDescriptionError = (repository, error) => ({
+    type: FETCH_DESCRIPTION_ERROR,
+    repository,
+    error
+});
 
+export const fetchDescription = repository => dispatch => {
+    debugger;
+    const url = `https://api.github.com/repos/${repository}`;
+    return fetch(url).then(response => {
+        if (!response.ok) {
+            const error = new Error(response.statusText)
+            error.response = response
+            throw error;
+        }
+        return response;
+    })
+    .then(response => response.json())
+    .then(data =>
+        dispatch(fetchDescriptionSuccess(repository, data.description))
+    )
+    .catch(error =>
+        dispatch(fetchDescriptionError(repository, error))
+    );
+};
 
 
--- js/reducers/index.js
+++ js/reducers/index.js
@@ -9,7 +9,7 @@ export const repositoryReducer = (state=initialRepositoryState, action) => {
             rating: null
         }];
     }
-    else if (action.type === actions.RATE_REPOSITORY) {
+    else if (action.type === actions.FETCH_DESCRIPTION_SUCCESS) {
         // Find the index of the matching repository
         const index = state.findIndex(repository =>
             repository.name === action.repository
@@ -21,7 +21,26 @@ export const repositoryReducer = (state=initialRepositoryState, action) => {
 
         const before = state.slice(0, index);
         const after = state.slice(index + 1);
-        const newRepository = Object.assign({}, state[index], {rating: action.rating});
+        const newRepository = Object.assign({}, state[index], {
+            description: action.description
+        });
+        return [...before, newRepository, ...after];
+    }
+    else if (action.type === actions.FETCH_DESCRIPTION_ERROR) {
+        // Find the index of the matching repository
+        const index = state.findIndex(repository =>
+            repository.name === action.repository
+        );
+
+        if (index === -1) {
+            throw new Error('Could not find repository');
+        }
+
+        const before = state.slice(0, index);
+        const after = state.slice(index + 1);
+        const newRepository = Object.assign({}, state[index], {
+            description: 'N/A'
+        });
         return [...before, newRepository, ...after];
     }
 
